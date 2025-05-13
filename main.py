27834002630e4a565564921f4206d4d44226aea0
jmerrill93 <merrill.joshua1@gmail.com>
2025-05-13 07:39:49 -0400

    working code to print text from a relative file.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def get_book_text(file_path):
+
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+   file_path = 'books/frankenstein.txt'
+   text = get_book_text(file_path)
+   print(text)
+
+main()
+
+   
+
+
+
+    
