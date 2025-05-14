91e6a4a31691be643f14015dfc83fa1bb2b083d3
jmerrill93 <merrill.joshua1@gmail.com>
2025-05-14 06:38:22 -0400

    main.py now counts number of words

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-   file_path = 'books/frankenstein.txt'
-   text = get_book_text(file_path)
-   print(text)
+    file_path = 'books/frankenstein.txt'
+    text = get_book_text(file_path)
+    return text
 
-main()
+def number_of_words(text):
+    words = text.split()
+    number = len(words)
+    print(f'{number} words found in the document')
+
+number_of_words(main())
 
    
 
